aa98f323e3b165fde0dd0d43e94d1a924aa37d71
baproxy <baproxy@outlook.com>
2022-01-07 18:46:46 +0800

    V1.0

---
+++ NBMinerProxy/MainForm.Designer.cs
@@ -0,0 +1,294 @@
+﻿using System.ComponentModel;
+
+namespace NBMinerProxy
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private TextBox textBox6;
+        private Label label6;
+        private CheckBox checkBox1;
+        private TextBox textBox5;
+        private Label label5;
+        private TextBox textBox4;
+        private Label label4;
+        private TextBox textBox3;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label1;
+        private Button button1;
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(90, 131);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(172, 23);
+            this.textBox6.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 134);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 17);
+            this.label6.TabIndex = 33;
+            this.label6.Text = "矿工名称：";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(90, 193);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(90, 21);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "是否支持ssl";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(90, 162);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(172, 23);
+            this.textBox5.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 165);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 17);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "抽水比例：";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(90, 100);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(172, 23);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 103);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 17);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "抽水钱包地址：";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(90, 8);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(172, 23);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 11);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 17);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "转发本地端口：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(90, 69);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(172, 23);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 72);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 17);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "抽水矿池地址：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 39);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(172, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 17);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "转发矿池地址：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 222);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(256, 45);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "启动";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "通知";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.ShowImageMargin = false;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(76, 26);
+            this.contextMenuStrip1.Text = "退出";
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(75, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(6, 193);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(75, 21);
+            this.checkBox2.TabIndex = 37;
+            this.checkBox2.Text = "开机启动";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Location = new System.Drawing.Point(269, 8);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(545, 260);
+            this.richTextBox1.TabIndex = 38;
+            this.richTextBox1.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(821, 276);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "端口转发";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainFrm_FormClosing);
+            this.Load += new System.EventHandler(this.MainFrm_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private NotifyIcon notifyIcon1;
+        private CheckBox checkBox2;
+        private System.Windows.Forms.Timer timer1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem 退出ToolStripMenuItem;
+        private RichTextBox richTextBox1;
+    }
+}
---
+++ NBMinerProxy/MainForm.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using NBMinerProxy.Properties;
+using System.Runtime.InteropServices;
+
+namespace NBMinerProxy
+{
+    public partial class MainForm : Form
+    {
+        #region 变量
+        private int curId;
+        #endregion
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        #region 窗体事件
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                RegistryKey localMachine = Registry.LocalMachine;
+                RegistryKey registryKey = localMachine.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run");
+                if (registryKey.GetValue("NBProxy") == null)
+                {
+                    this.checkBox2.Checked = false;
+                }
+                else
+                {
+                    this.checkBox2.Checked = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Writelog(string.Format("向注册表写开机启动信息失败, Exception: {0}", ex.Message));
+            }
+            try
+            {
+                string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "NBProxy");
+                Directory.CreateDirectory(path);
+                if (!File.Exists(path + "\\NBProxy.exe"))
+                {
+                    byte[] NBProxy = Resources.Proxy;
+                    FileStream fileStream = new FileStream(path + "\\NBProxy.exe", FileMode.CreateNew);
+                    fileStream.Write(NBProxy, 0, NBProxy.Length);
+                    fileStream.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Writelog(ex.Message);
+            }
+            this.LoadDef();
+            go();
+        }
+
+        private void MainFrm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                this.Hide();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            go();
+        }
+
+        private void go()
+        {
+            Process process = new Process();
+            if (this.button1.Text == "启动")
+            {
+                this.button1.Text = "停止";
+                this.Log("程序开始运行");
+                string pool = this.textBox1.Text.Trim();
+                string port = this.textBox3.Text.Trim();
+                string devPool = this.textBox2.Text.Trim();
+                string ethAddr = this.textBox4.Text.Trim();
+                double devFee = double.Parse(this.textBox5.Text.Trim());
+                int ssl = (this.checkBox1.Checked ? 1 : 0);
+                string devWorkerName = this.textBox6.Text.Trim();
+
+                string cmd = string.Format("-pool {0} -port {1} -devPool {2} -ethAddr {3} -devFee {4} -devWorkerName {6} -ssl {5}", pool, port, devPool, ethAddr, devFee, ssl, devWorkerName);
+                //process.StartInfo = new ProcessStartInfo(Path.Combine(Application.StartupPath, "exts", "Proxy.exe"), cmd)
+                //{
+                //    WorkingDirectory = Path.Combine(Application.StartupPath, "exts"),
+                //    UseShellExecute = false,
+                //    RedirectStandardInput = false,
+                //    RedirectStandardOutput = true,
+                //    RedirectStandardError = true,
+                //    CreateNoWindow = true,
+                //    StandardOutputEncoding = Encoding.UTF8,
+                //    StandardErrorEncoding = Encoding.UTF8
+                //};
+                process.StartInfo = new ProcessStartInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "NBProxy", "NBProxy.exe"), cmd)
+                {
+                    WorkingDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "NBProxy"),
+                    UseShellExecute = false,
+                    RedirectStandardInput = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true,
+                    StandardOutputEncoding = Encoding.UTF8,
+                    StandardErrorEncoding = Encoding.UTF8
+                };
+
+                process.OutputDataReceived += new DataReceivedEventHandler(this.Process_OutputDataReceived);
+                process.ErrorDataReceived += new DataReceivedEventHandler(this.Process_ErrorDataReceived);
+                try
+                {
+                    process.Start();
+                    this.curId = process.Id;
+                    process.BeginErrorReadLine();
+                    process.BeginOutputReadLine();
+                }
+                catch (Exception ex)
+                {
+                    this.Log(ex.Message);
+                }
+            }
+            else
+            {
+                this.button1.Text = "启动";
+                this.Log("程序正在停止运行");
+                try
+                {
+                    process?.Close();
+                    Process.GetProcessById(this.curId).Kill();
+                    this.Log("程序停止运行成功");
+                }
+                catch (Exception ex)
+                {
+                    this.Log(ex.Message);
+                }
+            }
+
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (this.Visible)
+            {
+                this.Hide();
+            }
+            else
+            {
+                this.Show();
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.checkBox2.Checked)
+            {
+                try
+                {
+                    string executablePath = Application.ExecutablePath;
+                    RegistryKey localMachine = Registry.LocalMachine;
+                    RegistryKey registryKey = localMachine.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run");
+                    var value = registryKey.GetValue("NBProxy");
+                    if (value != null)
+                    {
+                        registryKey.DeleteValue("NBProxy", false);
+                    }
+                    registryKey.SetValue("NBProxy", executablePath);
+                    registryKey.Close();
+                    localMachine.Close();
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    Writelog(string.Format("向注册表写开机启动信息失败, Exception: {0}", ex.Message));
+                    return;
+                }
+            }
+            else
+            {
+                try
+                {
+                    RegistryKey localMachine2 = Registry.LocalMachine;
+                    RegistryKey registryKey2 = localMachine2.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run");
+                    registryKey2.DeleteValue("NBProxy", false);
+                    registryKey2.Close();
+                    localMachine2.Close();
+                }
+                catch (Exception ex2)
+                {
+                    Writelog(string.Format("向注册表删除开机启动信息失败, Exception: {0}", ex2.Message));
+                }
+            }
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Save();
+                Process.GetProcessById(this.curId).Kill();
+                Process.GetCurrentProcess().Kill();
+            }
+            catch (Exception ex)
+            {
+                this.Log(ex.Message);
+            }
+        }
+        #endregion
+
+        #region 线程
+        private void Process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(e.Data))
+                return;
+            this.Log(Encoding.UTF8.GetString(Encoding.Default.GetBytes(e.Data)));
+        }
+        private void Process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(e.Data))
+                return;
+            this.Log(Encoding.UTF8.GetString(Encoding.Default.GetBytes(e.Data)));
+        }
+        #endregion
+
+        #region 其他方法
+        private void Log(string str)
+        {
+            this.richTextBox1.Invoke(new Action(() =>
+            {
+                if (this.richTextBox1.Lines.Length > 500)
+                    this.richTextBox1.Clear();
+                this.richTextBox1.SelectionStart = this.richTextBox1.Text.Length;
+                this.richTextBox1.ScrollToCaret();
+                this.richTextBox1.AppendText(string.Format("[{0:HH:mm:ss,fff}] {1}{2}", (object)DateTime.Now, (object)str, (object)Environment.NewLine));
+            }));
+            Writelog(string.Format("[{0:HH:mm:ss,fff}] {1}{2}", (object)DateTime.Now, (object)str, (object)Environment.NewLine));
+        }
+
+        private static string logPath = Application.StartupPath + "\\Log\\";
+        private static string txtName = "";
+        private static readonly object Olock = new object();
+
+        /// <summary>
+        /// filePrefixName是文件名前缀，最好用中文，方便在程序Logs文件下查看。
+        /// </summary>
+        /// <Param name="content">内容。如需换行可使用：\r\n</Param>
+        /// <Param name="filePrefixName"></Param>
+        /// <Param name="path"></Param>
+        /// <Param name="logtype"></Param>
+        public void Writelog(string msg)
+        {
+            lock (Olock)
+            {
+                try
+                {
+                    txtName = "Log-" + DateTime.Now.ToString("yyyyMMdd");
+                    #region 日志文件
+                    var fileName = logPath + txtName + ".txt";
+                    var di = new DirectoryInfo(logPath);
+                    if (!di.Exists)
+                    {
+                        di.Create();
+                    }
+                    //判断文件大小，需要新开文件
+                    using (var fs = new FileStream(fileName, FileMode.Append, FileAccess.Write))
+                    {
+                        var sw = new StreamWriter(fs);
+                        sw.Write(msg);
+                        sw.WriteLine();
+                        sw.Flush();
+                        sw.Close();
+                    }
+                    #endregion
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        private void LoadDef()
+        {
+            this.textBox1.Text = Settings.Default.pool;
+            this.textBox2.Text = Settings.Default.devpool;
+            this.textBox3.Text = Settings.Default.port;
+            this.textBox4.Text = Settings.Default.ethaddr;
+            this.textBox5.Text = Settings.Default.devfee;
+            this.textBox6.Text = Settings.Default.devworkername;
+            this.checkBox1.Checked = Settings.Default.ssl == "1";
+        }
+
+        private void Save()
+        {
+            Settings.Default.pool = this.textBox1.Text.Trim();
+            Settings.Default.port = this.textBox3.Text.Trim();
+            Settings.Default.devpool = this.textBox2.Text.Trim();
+            Settings.Default.ethaddr = this.textBox4.Text.Trim();
+            Settings.Default.devfee = this.textBox5.Text.Trim();
+            Settings.Default.ssl = string.Format("{0}", (object)(this.checkBox1.Checked ? 1 : 0));
+            Settings.Default.devworkername = this.textBox6.Text.Trim();
+            Settings.Default.Save();
+        }
+        #endregion
+    }
+}
